8c8f59765fb2637a22d7f8df0ce3d07b63d93f89
jonathanmoregard <jonathan.more@hotmail.com>
2026-04-28 14:35:29 +0200

    feat(secrets): add dellan host key to allKeys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,7 +5,10 @@ let
   # VM SSH host key (used by agenix to decrypt secrets at activation time)
   vm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJaYUR/n99axrFFFr/uv987jwaa6fYik7Ykf9iRSieZV root@nixos-vm";
 
-  allKeys = [ jonathan vm ];
+  # Dellan laptop SSH host key (used by agenix to decrypt secrets at activation time)
+  dellan = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJNvaYqBU7k/iTKPHcfVGYz5WJNVWnf0t26SX6Y7SZ0e root@dellan";
+
+  allKeys = [ jonathan vm dellan ];
 in {
   # Add secrets here as needed, e.g.:
   # "my-api-key.age".publicKeys = allKeys;
