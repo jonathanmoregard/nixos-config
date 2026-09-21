485f37c9bd40323ce5ea01d88699dcca2b86ef2a
jonathanmoregard <jonathan.more@hotmail.com>
2026-09-21 21:08:40 +0200

    secret(home-server): add smarthome deploy identity

--- hosts/home-server/deployment-identity.nix
+++ hosts/home-server/deployment-identity.nix
@@ -4,7 +4,14 @@
   homeServer = {
     ageHostPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEnGlRJufT9hIgzqFqHujW28DsSX1YDYg/0vGG7BsO1+ root@home-server";
     deployKeyFile = config.age.secrets.deploy-ssh-key.path;
+    smarthomeDeployKeyFile = config.age.secrets.smarthome-deploy-ssh-key.path;
   };
 
   age.secrets.deploy-ssh-key.rekeyFile = ../../secrets/deploy-ssh-key.age;
+  age.secrets.smarthome-deploy-ssh-key = {
+    rekeyFile = ../../secrets/smarthome-deploy-ssh-key.age;
+    owner = "root";
+    group = "root";
+    mode = "0400";
+  };
 }
